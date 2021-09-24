d0edd2b102ceb1f9f4211f33531259c3ad8c794c
witpawatwit <44453171+witpawat@users.noreply.github.com>
2021-09-24 13:33:54 +0700

    Add files via upload

---
+++ MedicalExamination.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalExamination extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    public function goat() {
+        return $this->belongsTo(Goat::class,'goat_id','goatId');
+    }
+}
